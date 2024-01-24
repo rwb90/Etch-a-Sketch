a7437d112d30c75407d9288a1fd8c9acf7c7203c
rwb90 <rwb90@miami.edu>
2024-01-23 22:41:37 -0500

    Added random RGB color with the option to choose

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
     const grid = document.querySelector('.grid');
     let multiple = 8;
     let buttons = document.querySelectorAll('button');
+    let color = "black";
     
     createGrid();
 
@@ -9,41 +10,61 @@
         for(let i = 0; i < (multiple ** 2); i++){
             let gridBox = document.createElement('div');
             gridBox.setAttribute('class', 'gridBox');
-            gridBox.setAttribute('style', 'outline: 2px solid black; aspect-ratio: 1/1; width: '+percentage+'%;');
+            gridBox.setAttribute('style', 'aspect-ratio: 1/1; width: '+percentage+'%;');
             
             grid.appendChild(gridBox);
         }
         let gridBoxes = document.querySelectorAll('.gridBox');
         gridBoxes.forEach((box) => {
         box.addEventListener('mouseover', () => {
-            box.style.backgroundColor = 'black';
+            
+            if(color == "random"){
+                box.style.backgroundColor = ''+random_rgba()+'';
+            }
+            else if(color == "black"){
+                box.style.backgroundColor = "black";
+            }
         }); 
         });
     }
-  
+    
+    function random_rgba() {
+        var o = Math.round, r = Math.random, s = 255;
+        return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + 1 + ')';
+    }
+    
     
 
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            while(grid.firstChild){
-                grid.removeChild(grid.lastChild);
+            if(button.parentElement.className == "gridButtons"){
+                while(grid.firstChild){
+                    grid.removeChild(grid.lastChild);
+                }
+                if(button.className == "8x8"){
+                    multiple = 8;
+                    createGrid();
+                }
+                else if(button.className == "16x16"){
+                    multiple = 16;
+                    createGrid();
+                }
+                else if(button.className == "32x32"){
+                    multiple = 32;
+                    createGrid();
+                }
+                else if(button.className == "64x64"){
+                    multiple = 64;
+                    createGrid();
+                }
             }
-            if(button.className == "8x8"){
-                multiple = 8;
-                createGrid();
+            else if(button.className == "black"){
+                color = "black";
             }
-            else if(button.className == "16x16"){
-                multiple = 16;
-                createGrid();
+            else if(button.className == "random"){
+                color = "random";
             }
-            else if(button.className == "32x32"){
-                multiple = 32;
-                createGrid();
-            }
-            else if(button.className == "64x64"){
-                multiple = 64;
-                createGrid();
-            }
+            
         });
     });
 
